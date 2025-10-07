3f65606fdf72ce09ff57b7821b2440ce3c876457
anshul shetty <anshulshetty82@gmail.com>
2025-10-07 21:47:21 +0530

    Update sketch.js

--- Day 2_Code in class_07.10.25/sketch.js
+++ Day 2_Code in class_07.10.25/sketch.js
@@ -1,6 +1,9 @@
+
+
 function setup() {
   createCanvas(500, 500);
   background(220);
+  let x = 0;
 }
 
 function draw() {
@@ -14,20 +17,33 @@ function draw() {
   //02 using othe inbuit variables, 
   // a) mouse - x,y
   //ellipse(mouseX,mouseY,100,50);
+
   // b) frame count 
   //noStroke()
   //fill(random(0,255),random(0,255),random(0,255),random(0,255));
   //ellipse (random(0,500),random(0,500),100,50);
+  
   // c) width of canvas
   //ellipseMode("center") 
   //fill("white")
   //ellipse(250,250,height/2,cos(frameCount/100)*300);
   //fill("red")
   //ellipse(250,250,20,20)
-  noStroke()
-  fill(mouseX/0.5,mouseY/2,mouseX/4);
-  ellipse(mouseX,mouseY,20,20);
-  ellipse(width-mouseX,mouseY,20,20);
+  //noStroke()
+  //fill(mouseX/0.5,mouseY/2,mouseX/4);
+  //ellipse(mouseX,mouseY,20,20);
+  //ellipse(width-mouseX,mouseY,20,20);
+  //ellipse(mouseX,height-mouseY,20,20);
+  //ellipse(width-mouseX,height-mouseY,20,20);
+
+  //animate our shapes - using variables 
+  //ellipseMode("center")
+  //ellipse(250,250,width,height);
+  //ellipseMode("center");
+  //ellipse(250,250,sin(frameCount/100)*500,sin(frameCount/100)*500);
+
+  ellipse(x ,100,100,50);
+
 
   
 
