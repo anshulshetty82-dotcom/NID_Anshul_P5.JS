22a42ff91a74047254184c910df1cecc4d1fc5a1
anshul shetty <anshulshetty82@gmail.com>
2025-10-07 23:23:47 +0530

    Update sketch.js

--- Day 2_Code in class_07.10.25/Project_2 Animation recreation_1/sketch.js
+++ Day 2_Code in class_07.10.25/Project_2 Animation recreation_1/sketch.js
@@ -1,7 +1,28 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
 }
 
 function draw() {
-  background(220);
+  noStroke();
+  background('#cfe2f5ff');
+
+  //face
+  fill(mouseX/3, 249, 166);
+  ellipse(250,250,300,300);
+
+  //eyes
+  fill(mouseX/3, 33, 32);
+  ellipse(180,210,15,20);
+  ellipse(width-180,210,15,20);
+
+  //cheeks
+  fill(241, 180, 225,mouseX/5);
+  ellipse(180,300,40,40);
+  ellipse(width-180,300,40,40);
+
+  //mouth 
+  fill("black");
+  ellipse(250,320,mouseX/50,20);
+
+
 }
