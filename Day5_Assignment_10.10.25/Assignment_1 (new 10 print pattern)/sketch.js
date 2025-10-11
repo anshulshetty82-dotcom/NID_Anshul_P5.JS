cc61c15768504c3a32fd56e0dfd02d3ab092c578
anshul shetty <anshulshetty82@gmail.com>
2025-10-12 03:16:52 +0530

    12.10.25

---
+++ Day5_Assignment_10.10.25/Assignment_1 (new 10 print pattern)/sketch.js
@@ -0,0 +1,29 @@
+let size = 150;
+let myPattern=[];
+let noImages=4;
+
+function preload(){
+  for(let i=0; i < noImages;i=i+1){
+  let name = "Images/g"+i+".jpg";
+  myPattern[i]= loadImage(name);
+  }
+}
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(1);
+}
+
+function draw() {
+  background(220);
+
+  for(let i=0; i<width; i=i+size){
+    for(let j=0; j<height; j=j+size){
+      let choice = floor(random(0,noImages));
+      image(myPattern[choice],i,j,size+2,size+2);
+    }
+  }
+
+}
---
+++ Day5_Assignment_10.10.25/Assignment_2 (Sprite animation)/sketch.js
@@ -0,0 +1,107 @@
+let spriteImage, sprites = [];
+let spriteX = 10, spriteY = 7;
+let count = 0;
+let row = 0;
+let x = 0, y = 0;
+let xdir = 0, ydir = 0;
+let k, l, m;
+let capX = 0;
+
+
+function preload() {
+
+  spriteImage = loadImage('Images/g0.png');
+
+}
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+
+  
+
+
+  let w = spriteImage.width / spriteX;
+  let h = spriteImage.height / spriteY;
+
+
+  for (let i = 0; i < spriteY; i++) {
+
+    sprites[i] = [];
+
+
+    for (let j = 0; j < spriteX; j++) {
+
+      sprites[i][j] = spriteImage.get(j * w, i * h, w, h);
+    }
+  }
+
+}
+
+function draw() {
+  background(255);
+  fill("black")
+  noStroke();
+  rect(0,innerHeight/1.482,width,innerHeight);
+  // noStroke();
+
+  // fill(167, 199, 231,50);
+  // triangle(300, 300, 0, 512, 1000,700);
+
+  // fill(167, 199, 231,50);
+  // triangle(300, 300, 230, 350, 388,350);
+
+  // fill('red');
+  // ellipse(1300, 150, 250,250);
+
+  // fill(50, 100, 255);
+  // triangle(1000, 50, 300, 512, 1900,512);
+
+  // fill(167, 199, 231,60);
+  // triangle(1000, 50, 800, 182, 1257,182);
+  
+  // fill(14, 14, 14);
+  // rect(0, 512, innerWidth, 300);
+
+  image(sprites[row][count], x, height/2,200,200);
+  if (frameCount % 5 == 0) {
+    count = (count + 1) % spriteX;
+    x = x + xdir;
+    y = y + ydir;
+  }
+}
+
+function keyPressed() {
+  if (keyIsDown(RIGHT_ARROW) && keyIsDown(UP_ARROW)) {
+    row = 3;
+    xdir = 14;
+    ydir = 0;
+  } else if (keyIsDown(LEFT_ARROW) && keyIsDown(UP_ARROW)) {
+    row = 4;
+    xdir = -14;
+    ydir = 0;
+  }  else if (keyCode == 65) {
+    row = 5;
+    xdir = 0;
+    ydir = 0;
+  } else if (keyCode == LEFT_ARROW) {
+    row = 2;
+    ydir = 0;
+    xdir = -7;
+  } else if (keyCode == RIGHT_ARROW) {
+    row = 1;
+    ydir = 0;
+    xdir = 7;
+  } else if (keyCode == 83) {
+    row = 6;
+    ydir = 0;
+    xdir = 0;
+  }
+}
+function keyReleased() {
+  row = 0;
+  xdir = 0;
+  ydir = 0;
+  count = frameCount % 12;
+  capX = 10;
+
+}
