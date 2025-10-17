e76e56f68a7cd652c86211121ad3f7250d498e07
anshul shetty <anshulshetty82@gmail.com>
2025-10-17 10:25:24 +0530

    17.10.25_final cut game.

--- Day9_Game prototype_16.10.25/Code in class_Game prototype_1/sketch.js
+++ Day9_Game prototype_16.10.25/Code in class_Game prototype_1/sketch.js
@@ -1,5 +1,5 @@
 //gamestatearray
-let gameStatearray = ["intro", "level1", "level2", "level3"]
+let gameStatearray = ["startscreen","intro", "level1", "level2", "level3"]
 
 //array var 
 let gno = 0;
@@ -10,13 +10,17 @@ let currentGamestate = gameStatearray[0];
 //image var 
 //image - for level 1
 let bg1, l1g1, l1f1
-
 //image - for level 2
 let bg2, l2f1;
+//image - for level 3 
+let bg3, l3f1;
+//image - for level 3 
+let bg4, l4f1;
 
 //player movement var
 let x = 0, y = 0;
 let xdir = 0, ydir = 0;
+let playerspeed = 10;
 
 //animation control 
  let sprites = [];
@@ -34,10 +38,14 @@ function preload() {
   bg1 = loadImage("images/1.png");
   l1g1 = loadImage("images/2.png");
   l1f1 = loadImage("images/3.png");
-
   //level 2 images 
   bg2 = loadImage("images/4.jpg");
   l2f1 = loadImage("images/5.png");
+  //level 3 images 
+  bg3 = loadImage("images/6.jpg")
+  l3f1 = loadImage('images/8.png')
+  //level 4 images 
+  bg4 = loadImage("images/7.jpg")
 
   //player sprite sheet 
   spriteImage = loadImage('images/g0.png');
@@ -51,7 +59,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  gameSoundbackground.setVolume(0.5);
+  gameSoundbackground.setVolume(0.2);
 
   //sprite cutting, r = start animation, coll = anim fame, 
   let w = spriteImage.width / spriteX;
@@ -67,22 +75,36 @@ function setup() {
 }
 
 function draw() {
-  if (currentGamestate == gameStatearray[0]) {
-    introScreen();
-  } else if (currentGamestate == gameStatearray[1]) {
+  if(currentGamestate == gameStatearray[0] ){
+    startScreen();
+  }else if (currentGamestate == gameStatearray[1]) {
     levelOne();
+  } else if (currentGamestate == gameStatearray[2]) {
+    levelTwo();
+  } else if(currentGamestate== gameStatearray[3]){
+    levelThree();
+  } else if(currentGamestate == gameStatearray[4]){
+    levelFour();
   }
 }
 
 // ------------------------------LEVEL THINGS ------------------------------------------------------------------
-function introScreen() {
+function startScreen(){
+  fill("BLACK");
+  rect(0,0,width,height);
+
+}
+
+function levelOne() {
   //bgsound 
   //gameSoundbackground.loop();
 
   //background
   image(bg1, 0, 0, width, height);
-  // text on screen 
-  textOnscreenLevelone();
+
+
+
+
   //ground plane
   image(l1g1, 0, 0, width, height);
 
@@ -96,9 +118,12 @@ function introScreen() {
   //foreground
   image(l1f1, 0, 0, width, height);
 
+    // text on screen 
+  textOnscreenLevelone();
+
 }
 
-function levelOne() {
+function levelTwo() {
 
   //background 
   image(bg2, 0, 0, width, height);
@@ -117,6 +142,40 @@ function levelOne() {
 
 }
 
+function levelThree(){
+  //background 
+  image(bg3, 0, 0, width, height);
+
+    //ground plane
+  image(l1g1, 0, 0, width, height);
+
+  //player functions 
+  player();
+  playerMovement();
+
+   //foreground
+  image(l3f1, 0, 0, width, height);
+
+
+  textOnscreenLevelthree();
+
+}
+
+function levelFour(){
+    //background 
+  image(bg4, 0, 0, width, height);
+
+    //ground plane
+  image(l1g1, 0, 0, width, height);
+
+  //player functions 
+  player();
+  playerMovement();
+
+  textOnscreenLevelfour();
+
+
+}
 // ----------------------------- PLAYER RELATED THINGS ---------------------------------------------------------
 
 function player() {
@@ -146,7 +205,7 @@ function playerMovement() {
   if (keyIsDown(RIGHT_ARROW)) {
     row = 1;
     ydir = 0;
-    xdir = +7;
+    xdir = +playerspeed;
 
     if (!playerWalksound.isPlaying()) {      
             playerWalksound.loop(); 
@@ -155,7 +214,7 @@ function playerMovement() {
   } else if (keyIsDown(LEFT_ARROW)) {
     row = 2;
     ydir = 0;
-    xdir = -7;
+    xdir = -playerspeed;
     
         if (!playerWalksound.isPlaying()) {      
             playerWalksound.loop(); 
@@ -174,7 +233,7 @@ function playerMovement() {
     gno = gno + 1;
     currentGamestate=gameStatearray[gno];
     x = 0;
-    print(gno);
+    //print(gno);
 
   } else if (x < 0) {
     gno = gno - 1
@@ -211,18 +270,23 @@ function textOnscreenLevelone(){
     
     let tr = map(x,0,width/2,0,255);
     textAlign(CENTER);
-    fill(0,0,0,tr);
-    textSize(100);
-    text("Where am i.", width/2,height/2);
+    fill(255,255,255,tr);
+    textSize(40);
+    strokeWeight(40);
+    textFont('Courier New');
+    text("Where am i, what is this place.", width/2,height-70);
   } 
 
   if (x > width/2 && x < width){
     let tr = map(x,width/2,width,0,255);
     textAlign(CENTER);
-    fill(0,0,0,tr);
-    textSize(100);
-    text("Who am i.", width/2,height/2);
+    fill(225,225,225,tr);
+    textSize(40);
+    textFont('Courier New');
+    text("Is there a path beyond, I feel cold...", width/2,height-70);
     }
+
+    
 }
 
 function textOnscreenLevetwo(){
@@ -231,19 +295,55 @@ function textOnscreenLevetwo(){
     
     let tr = map(x,0,width/2,0,255);
     textAlign(CENTER);
-    fill(0,0,0,tr);
-    textSize(100);
-    text("Where am i.", width/2,height/2);
+    fill(225,225,225,tr);
+    textSize(60);
+    text("These voices !!", width/2,height-70);
   } 
 
   if (x > width/2 && x < width){
     let tr = map(x,width/2,width,0,255);
     textAlign(CENTER);
-    fill(0,0,0,tr);
-    textSize(100);
-    text("Who am i.", width/2,height/2);
+    fill(225,225,225,tr);
+    textSize(40);
+    text("Aaahh! It's deafening. Go away!", width/2,height-70);
     }
 }
+
+function textOnscreenLevelthree(){
+
+  if (x > 10  && x < width){
+    
+    let tr = map(x,0,width,0,255);
+    textAlign(CENTER);
+    fill(225,225,225,tr);
+    textSize(70);
+    text("They follow me.", width/2,height-70);
+  } 
+
+  // if (x > width/2 && x < width){
+  //   let tr = map(x,width/2,width,0,255);
+  //   textAlign(CENTER);
+  //   fill(225,225,225,tr);
+  //   textSize(100);
+  //   text("Who am i.", width/2,height-70);
+  //   }
+}
+function textOnscreenLevelfour(){
+
+  if (x > 10  && x < width/2){
+    
+    let tr = map(x,0,width,0,255);
+    textAlign(CENTER);
+    fill(225,225,225,tr);
+    textSize(70);
+    text("I must choose now !", width/2,height-70);
+  } 
+
+}
+
+
+// i used ai for this part, i could not understand the logic, and it was messing up my game. something with 
+// loop had to do with it 
 
 function mouseClicked() {
     // 1. Check if the audio context is running (p5.js handles this)
@@ -256,4 +356,9 @@ function mouseClicked() {
     
     // You can also place the first movement/level start logic here if you want
     // the game to start only after the first click.
+
+    if(currentGamestate == gameStatearray[0]){
+     gno = gno+1;
+     currentGamestate = gameStatearray[gno];
+    } 
 }
